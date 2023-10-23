9572af7a6f5d6c2a26365c6e06cd5a40a51e8df7
juanmaherrera94 <juanmah2911@gmail.com>
2023-10-23 16:17:47 +0200

    commit ejercicio8

---
+++ UD3/procesarejercicio8.php
@@ -0,0 +1,18 @@
+<?php
+function recoge($var) {
+    $tmp ="";
+    if (isset($_REQUEST[$var])) {
+        $tmp = trim(htmlspecialchars($_REQUEST[$var]));
+     }   
+     return $tmp;
+}
+$nombre = recoge("nombre");
+$apellidos = recoge("apellidos");
+$nombreBien=false;
+$apellidoBien=false;
+if(empty($nombre) || empty($apellidos)){
+ echo "Los campos no pueden estar vacios";
+}else{
+    echo "Su nombre es $nombre y su apellido es $apellidos";
+}
+?>
